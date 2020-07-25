85ff7d85f9d7324bce60767fffd424ed6e69bea8
Gargoth <51285956+Gargoth@users.noreply.github.com>
2020-07-26 03:28:31 +0800

    Added Random AI

--- main.py
+++ main.py
@@ -1,16 +1,24 @@
+from random import randrange
+
 class Tictactoe:
-    def __init__(self, size):
+    def __init__(self, size, ai=True):
         self.size = size
         self.board = self.create_board(self.size)
+        self.ai = ai
 
     def print_board(self):
         for i in self.board:
             ret = list(map(lambda a: str(a).zfill(len(str(self.size ** 2 - 1))), i))
             print(ret)
 
-    def play_game(self, ai=False):
+    def play_game(self):
         turn_counter = 0
         players = ["X", "O"]
+        
+        if self.ai:
+            ai_player = players[randrange(2)]
+
+        print(f"AI plays {ai_player}")
 
         while not self.get_result():
             current_player = players[turn_counter % 2]
@@ -19,11 +27,14 @@ class Tictactoe:
                 self.print_board()
                 print("\n\n")
                 try:
-                    cell_input = int(
-                        input(
-                            f"Type which cell to place in (Player {current_player}): \n"
+                    if self.ai and ai_player == current_player:
+                        cell_input = self.minmaxmove()
+                    else:
+                        cell_input = int(
+                            input(
+                                f"Type which cell to place in (Player {current_player}): "
+                            )
                         )
-                    )
                     board_row = self.board[int(cell_input) // self.size]
                     cell_index = board_row.index(cell_input)
                     if board_row[cell_index] is not int:
@@ -62,6 +73,9 @@ class Tictactoe:
             return diagonal_up[0]
         return False
 
+    def minmaxmove(self):
+        return randrange(self.size**2)
+
     @staticmethod
     def create_board(size=3):
         board = []
@@ -72,11 +86,5 @@ class Tictactoe:
         return board
 
 
-class MinMaxAI:
-    def __init__(self):
-        pass
-
-
 game = Tictactoe(3)
 game.play_game()
-
